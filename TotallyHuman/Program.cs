2ddfc93b0e5c6d29493295d234d6e0b8d6ec12e5
Stelios Klapsis <Steliosklapsis@outlook.com>
2023-03-26 21:21:53 -0400

    Basic Bot Setup

--- TotallyHuman/Program.cs
+++ TotallyHuman/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace TotallyHuman
+using Discord;
+using Discord.WebSocket;
+
+namespace TotallyHuman
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello, World!");
+            DiscordSocketClient client = new DiscordSocketClient();
+
+            client.Log += Log;
+
+            string token = File.ReadAllText(@"..\..\..\..\Token.txt");
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
